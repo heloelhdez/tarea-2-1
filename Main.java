d1fc424d4078f7c9fa40aaaf09208461ec66b0dd
Heloel Hernández Santos <heloelhdez@hotmail.com>
2017-10-05 23:18:35 -0500

    Listo :D

--- Main.java
+++ Main.java
@@ -6,7 +6,8 @@
 
 //Importacion de librerias de Java
 import java.util.Scanner;
-
+import java.util.Arrays;
+import java.util.ArrayList;
 class Main{
 
 	public static void main(String[] args) {
@@ -33,8 +34,9 @@ class Main{
 		//Generamos valores pseudoaleatorios hasta cumplir con la cantidad solicitada o se encuentra un ciclo
         for (int i = 1; i< cuantos && hayCiclo == false; i++) {
         	xn=( ( a * xn + c ) % m); //El siguiente numero pseudoaleatorio se encuentra y queda como semilla para el siguiente numero
-			arrCuantos[i]=xn;
 			
+			arrCuantos[i]=xn;
+
 			//iteramos todo el arreglo de numeros aleatorios para saber si existe un ciclo
         	for (int j = 0; j < i; j++) {
         		if (arrCuantos[i] == arrCuantos[j]) { //existe un ciclo en el arreglo
@@ -111,8 +113,18 @@ class Main{
 		int cantidadPeriodo = cola.length + ciclo.length;
 		System.out.println("Cantidad de elementos: " + cantidadPeriodo);
 
-		//TODO: Calcular Media, Mediana, Moda, Varianza y Desviacion Estandar (Heloel)
+		
+		//Ordenamos el arreglo para poder obtener la mediana
+		bubbleSort(arrCuantos);
 		System.out.println("--------------");
+		//Llamadas e impresiones de los metodos de Media, Mediana, Moda, Varianza y Desviacion Estandar
+		System.out.println("El promedio es: "+media(arrCuantos));
+		System.out.println("La mediana es: "+mediana(arrCuantos));
+		System.out.print("La moda es: ");
+		moda(arrCuantos);
+		System.out.println("La varianza es: "+varianza(arrCuantos));
+		System.out.println("La desviacion estandar es: "+desviacionEstandar(arrCuantos));
+
 
 		//Declaracion de variables para llevar un conteo de los rangos
 		double rango01 = 0, rango12 = 0, rango23 = 0, rango34 = 0, rango45 = 0, rango56 = 0, rango67 = 0, rango78 = 0, rango89 = 0, rango91 = 0;
@@ -172,5 +184,85 @@ class Main{
 		System.out.println("--------------");
 		System.out.println("FIN DEL PROGRAMA");
 
+	}
+	//Metodo Bubble Sort para ordenar el arreglo
+	public static void bubbleSort(double[] arreglo) {
+    int contador = 0;
+    double auxiliar = 0;
+    boolean intercambiado = true;
+	    
+	    while (intercambiado) {
+
+	        intercambiado = false;
+	        contador++;
+	        
+	        for (int i = 0; i < arreglo.length - contador; i++) {
+	        
+	            if (arreglo[i] > arreglo[i + 1]) {
+	                auxiliar = arreglo[i];
+	                arreglo[i] = arreglo[i + 1];
+	                arreglo[i + 1] = auxiliar;
+	                intercambiado = true;
+	            	}
+	        
+	        	}
+	    	
+	    	}
+	}
+	//Metodo para obtener la mediana
+	public static  double mediana(double[] arreglo){
+		double mediana;
+		if (arreglo.length % 2 == 0)
+		    mediana = ((double)arreglo[arreglo.length/2] + (double)arreglo[arreglo.length/2 - 1])/2;
+		else
+		    mediana = (double) arreglo[arreglo.length/2];
+		return mediana;
+	}
+	//Metodo para obtener la Media
+	public static double media(double[] arreglo){
+		double suma = 0;
+		for (double valor : arreglo) 
+			suma += valor;
+		double media = 1.0d * suma / arreglo.length;
+		return media;
+	}
+	//Metodo para obtener las Modas
+	public static void moda(double arreglo[]){
+
+	  ArrayList<Double> moda = new ArrayList<Double>();
+
+	  int maximo=0;   
+	  for (int i = 0; i < arreglo.length; i++){
+	    int contador = 0;
+	    for (int j = 0; j < arreglo.length; j++){
+	      if (arreglo[j] == arreglo[i]) 
+	      	contador++;
+	    }
+	    if (contador > maximo){
+	      maximo = contador;
+	      moda.clear();
+	      moda.add(arreglo[i]);
+	    } else 
+	    	if (contador == maximo){
+		      moda.add(arreglo[i]);
+		    }
+	  }
+	  System.out.println(Arrays.toString(moda.toArray()));
+	}
+	//Metodo para obtener la varianza
+	public static double varianza(double[] arreglo){
+		 double diferenciasCuadradas = 0.0;
+		   double promedio = media(arreglo);
+		   for (double valor : arreglo)
+		   {
+		       double diferencia = valor - promedio;
+		       diferencia *= diferencia;
+		       diferenciasCuadradas += diferencia;
+		   }
+		   return (diferenciasCuadradas  / (arreglo.length-1));
+	}
+	//Metodo para obtener la desviacion estandar
+	public static double desviacionEstandar(double[] arreglo){
+		return (double)Math.pow(varianza(arreglo), 0.5);
 	}
 }
